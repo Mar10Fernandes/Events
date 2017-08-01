f2d105a31b4a53352151ad809b0e82759e25046b
José Gomes <jose.gomes@egplusww.com>
2017-08-01 22:05:25 +0100

    test

---
+++ Service/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Entities;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services
+{
+    public class Data
+    {
+        //public IList<Event> GetEvent()
+        //{
+            
+        //}
+    }
+}
